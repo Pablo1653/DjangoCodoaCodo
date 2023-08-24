a5b6c4a4f87de0c817cd050040d2165f74ff170f
Pablo1653 <fsanguin@agro.uba.ar>
2023-08-24 12:45:11 -0300

    Commit del proyecto django

---
+++ ventas/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+
+from . import views
+
+urlpatterns=[
+
+    path('ventas/listadeventas/', login_required(views.ListaDeVentas.as_view()), name='listadeventas'),
+    path('ventas/altaventaform', login_required(views.altaventaform.as_view()), name='altaventaform'),
+   # path('ventas/bajaventaform', login_required(views.bajaventaform.as_view()), name="bajaventaform"),
+   # path('ventas/ventaeditar/<int:id_venta>', login_required(views.ventaeditar), name='ventaeditar'),
+   # path('ventas/ventaeliminar/<int:id_venta>', login_required(views.ventaeliminar), name='ventaeliminar'),
+    path('ventas/paginaenblanco/', login_required(views.paginaenblanco), name='paginaenblanco'),
+
+    path('ventas/buscarventas/', login_required(views.buscarventas), name='buscarventas'),
+    
+]
---
+++ ventas/views.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from django.shortcuts import render, redirect
+#from clientes.models import Cliente
+#from pagoformas.models import PagoForma
+from django.views import View
+from django.views.generic import ListView
+
+from ventas.forms import VentasForm, AltaVentaForm,BuscarVentaForm
+from ventas.models import Venta
+
+
+
+def paginaenblanco(request):
+    context = {"hoy": datetime.now}
+    return render(request, 'ventas/paginaenblanco.html', {"context": context})
+
+# def estadisticas(request):
+#     context = {"hoy": datetime.now}
+#     return render(request, 'ventas/estadisticas.html', {"context": context})
+
+def buscarventas(request): #filtro por nombre
+    ventas = Venta.objects.all()
+    ventas=ventas.filter(fecha_de_venta__range=["1900-09-01", "2022-10-31"])
+    context={
+     'form': BuscarVentaForm(),
+     'ventas':ventas,
+    }
+    return render(request,'ventas/buscarventas.html',context)
+
+class ListaDeVentas(ListView):
+    model = Venta 
+    context_object_name = 'ventas'
+    template_name = 'ventas/listadeventas.html'
+    ordering =['id']
+
+
+class altaventaform(View): #FORMULARIO DE ALTA
+    form_class = AltaVentaForm
+    template_name = 'ventas/altaventaform.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'formulario': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listadeventas')
+
+        return render(request, self.template_name, {'formulario': form})
